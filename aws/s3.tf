9e2edd68cd5b694d26f8b3c30b70f53255eb9b8e
Xnuk Shuman <admin@xnu.kr>
2023-12-23 13:12:22 +0900

    apply s3 bucket policies - maybe manually

--- aws/s3.tf
+++ aws/s3.tf
@@ -6,12 +6,6 @@ import {
 
 import {
   provider = aws.seoul
-  to       = aws_s3_bucket_public_access_block.file
-  id       = "file.xnu.kr"
-}
-
-import {
-  provider = aws.seoul
   to       = aws_s3_bucket.kobis
   id       = "kobis.xnu.kr"
 }
@@ -34,39 +28,78 @@ import {
   id       = "www.xnu.kr"
 }
 
+locals {
+  s3-meta = {
+    file = {
+      s3         = aws_s3_bucket.file
+      cloudfront = aws_cloudfront_distribution.file
+    }
+    kobis = {
+      s3         = aws_s3_bucket.kobis
+      cloudfront = aws_cloudfront_distribution.kobis
+    }
+    trunk = {
+      s3         = aws_s3_bucket.trunk
+      cloudfront = aws_cloudfront_distribution.trunk
+    }
+    trunk-gotosocial = {
+      s3         = aws_s3_bucket.trunk-gotosocial
+      cloudfront = aws_cloudfront_distribution.trunk
+    }
+    www = {
+      s3         = aws_s3_bucket.www
+      cloudfront = aws_cloudfront_distribution.www
+    }
+  }
+}
+
 
 resource "aws_s3_bucket" "file" {
   provider = aws.seoul
   bucket   = "file.xnu.kr"
 }
 
-data "aws_iam_policy_document" "file_xnu_kr_policy" {
+data "aws_iam_policy_document" "s3-cloudfront-policies" {
+  for_each = local.s3-meta
+
   statement {
-    sid       = "AllowCloudFrontServicePrincipal"
-    effect    = "Allow"
+    sid    = "CloudfrontConnectionManagedByTerraform"
+    effect = "Allow"
+
+    principals {
+      # TODO: "www" uses OAI (legacy)
+      type = each.key == "www" ? "AWS" : "Service"
+      identifiers = (
+        each.key == "www"
+        ? ["arn:aws:iam::cloudfront:user/CloudFront Origin Access Identity E2G9C35NHLB4O4"]
+        : ["cloudfront.amazonaws.com"]
+      )
+    }
+
     actions   = ["s3:GetObject"]
-    resources = ["${aws_s3_bucket.file.arn}/*"]
+    resources = ["${each.value.s3.arn}/*"]
+
     condition {
       test     = "StringEquals"
       variable = "AWS:SourceArn"
-      values   = [aws_cloudfront_distribution.file.arn]
-    }
-    principals {
-      type        = "Service"
-      identifiers = ["cloudfront.amazonaws.com"]
+      values   = [each.value.cloudfront.arn]
     }
   }
 }
 
-resource "aws_s3_bucket_policy" "file_policy" {
+resource "aws_s3_bucket_policy" "policies" {
+  for_each = local.s3-meta
+
   provider = aws.seoul
-  bucket   = aws_s3_bucket.file.id
-  policy   = data.aws_iam_policy_document.file_xnu_kr_policy.json
+  bucket   = each.value.s3.bucket
+  policy   = data.aws_iam_policy_document.s3-cloudfront-policies[each.key].json
 }
 
-resource "aws_s3_bucket_public_access_block" "file" {
+resource "aws_s3_bucket_public_access_block" "make-private" {
+  for_each = local.s3-meta
+
   provider = aws.seoul
-  bucket   = aws_s3_bucket.file.id
+  bucket   = each.value.s3.bucket
 
   block_public_acls       = true
   block_public_policy     = true
